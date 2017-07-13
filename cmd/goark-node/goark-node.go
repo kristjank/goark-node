e1719e25a41893fc9ceb46cc62222502e44c4b45
chris <kristjan.kosic@adora-med.com>
2017-07-13 15:21:39 +0200

    Moved routes method

--- cmd/goark-node/goark-node.go
+++ cmd/goark-node/goark-node.go
@@ -56,6 +56,41 @@ func initializeBoltClient() {
 	api.DBClient.InitializeBucket()
 }
 
+func initializeRoutes() {
+	log.Info("Initializing routes")
+
+	// TODO implement checkHeaders function to check the correct header data
+	// indicating whether the request was from an authenticated peer? or not
+	// Handle the index route
+	//router.GET("/")
+
+	// Group peer related routes together
+	peerRoutes := router.Group("/peer")
+	{
+		peerRoutes.GET("/list", api.GetTransactions)
+		peerRoutes.GET("/status", api.SendPeerStatus)
+
+		peerRoutes.POST("/blocks", api.ReceiveBlocks)
+		peerRoutes.POST("/transactions", api.ReceiveTransactions)
+	}
+
+	transactionRoutes := router.Group("/api/transactions")
+	{
+		// Handle the GET requests at /peer/list
+		transactionRoutes.GET("", api.GetTransactions)
+	}
+
+	apiRoutes := router.Group("/api/blocks")
+	{
+		apiRoutes.GET("/getHeight", api.GetHeight)
+	}
+
+	autoconfigureRoutes := router.Group("/api/loader")
+	{
+		autoconfigureRoutes.GET("/autoconfigure", api.GetAutoConfigureParams)
+	}
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////
 func main() {
 	log.Info("---- GOARK Relay Node Starting ----")
