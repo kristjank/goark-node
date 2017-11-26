77acb550f395701f92da3059d7161e047aa18f88
chris <kristjan.kosic@gmail.com>
2017-11-26 02:12:51 +0100

    hook model adjusted

--- test/hooks/hooks.go
+++ test/hooks/hooks.go
@@ -12,18 +12,19 @@ var hookReader *viper.Viper
 
 type HooksConfig struct {
 	Hooks []struct {
-		HookID      string `json:"hook-id,omitempty"`
-		CallbackURI string `json:"callbackURI,omitempty"`
-		Conditions  struct {
-			RecipientID string `json:"RecipientID,omitempty"`
-			Amount      int    `json:"Amount,omitempty"`
-			Asset       string `json:"Asset,omitempty"`
-			Type        int    `json:"Type,omitempty"`
-			VendorField string `json:"VendorField,omitempty"`
-			Height      int    `json:"Height,omitempty"`
-			SenderID    string `json:"SenderID,omitempty"`
-		} `json:"conditions,omitempty"`
-	} `json:"hooks,omitempty"`
+		HookID      string
+		CallbackURI string
+		HookType    string
+		TriggerRule struct {
+			RecipientID string
+			Amount      int
+			Asset       string
+			Type        int
+			VendorField string
+			Height      int
+			SenderID    string
+		}
+	}
 }
 
 func initHooksConfig() {
@@ -43,7 +44,7 @@ func initHooksConfig() {
 	}
 
 	for _, el := range cfg.Hooks {
-		fmt.Println(el.Conditions.RecipientID)
+		fmt.Println(el.TriggerRule.RecipientID)
 	}
 
 	fmt.Println(hookReader.AllSettings())
